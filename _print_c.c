db17ce1dc7ca642e93214c38b7d968ca1808bcd8
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-17 08:22:10 -0700

    Push Print Chararater

---
+++ _print_c.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_char - prints a character
+ * @c: character parameter 
+ * Return: 1
+ */
+
+int _print_char(va_list c)
+{
+	char _c;
+
+	_c = va_arg(c, int);
+	_putchar(_c);
+
+	return (1);
+}
--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-
+int _print_char(va_list c);
+int _putchar(char c);
 
 #endif
