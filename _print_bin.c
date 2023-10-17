bd56959c6e5b4c8b64323e937fab2299b4748607
DANIEL AMOAKO KODUA <officialjwise20@gmail.com>
2023-10-17 13:39:01 -0700

    More files and functions

--- _print_bin.c
+++ _print_bin.c
@@ -7,17 +7,17 @@
 
 int _print_bin(va_list val)
 {
-        int flag = 0;
-        int cont = 0;
-        int i, a = 1, b;
-        unsigned int num = va_arg(val, unsigned int);
-        unsigned int p;
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
 
-        for (i = 0; i < 32; i++)
-        {
-                p = ((a << (32 - i))  && num);
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (32 - i))  && num);
 		if (p >> (31 - i))
-			flag = 1;
+		flag = 1;
 		if (flag)
 		{
 			b = p >> (31 - i);
