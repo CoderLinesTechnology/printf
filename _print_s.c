8c4a6ef0b7b7d6841d011366c42a8d4bb1b16e6e
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-17 15:38:58 +0000

    Push Print String

---
+++ _print_s.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*_print_s - Print a string
+*val: String value parameter
+*Return: length of the string
+*/
+
+int _print_s(va_list val)
+{
+	char *str;
+	int i, length;
+
+	str = va_arg(val, char *);
+	if (str == NULL)
+	{
+		str = "\0";
+		return (1);
+	}
+	else
+	{
+		length = _strlen(str);
+		for (i = 0; i < length; i++)
+		_putchar(str[i];
+		return (length);
+	}
+
+
