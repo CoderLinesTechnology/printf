4e35b65bc1ee6d105ea674c4bf72b6927bbf105b
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-17 19:04:45 +0000

    Added pointer

---
+++ _print_pointer.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+int _print_pointer(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	int i, b;
+	long int a;
+
+	p = va_arg(val, void *);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+			_putchar(s[i]);
+		return (i);
+	}
+	a = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	b = _print_hex_extra(a);
+}
